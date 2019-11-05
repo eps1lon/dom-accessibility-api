101fbafebf2709f90c9a44fa6f56fc38f38cbb56
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2019-11-05 22:00:41 +0100

    chore: Use named function for presentational check

--- sources/accessible-name.ts
+++ sources/accessible-name.ts
@@ -454,7 +454,7 @@ export function computeAccessibleName(
 			}
 
 			// 2D
-			if (!hasAnyConcreteRoles(current, ["none", "presentation"])) {
+			if (!isMarkedPresentational(current)) {
 				const elementTextAlternative = computeElementTextAlternative(current);
 				if (elementTextAlternative !== null) {
 					consultedNodes.add(current);
