3a6d68952ce0c8b80a392a11fd9755ef11406a5d
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-03-21 19:14:55 +0100

    fix jest tests

--- .babelrc.js
+++ .babelrc.js
@@ -1,6 +1,11 @@
 module.exports = {
 	presets: [
-		[require.resolve("@babel/preset-env"), { modules: false }],
+		[
+			require.resolve("@babel/preset-env"),
+			// for jest we need to transpile esmodules
+			// otherwise transform-commonjs takes care of the correct module syntax
+			{ modules: process.env.NODE_ENV === "test" ? "auto" : false }
+		],
 		require.resolve("@babel/preset-typescript")
 	],
 	env: {
