0897630862d608a9ca22e9799bb30b37e1032afa
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-03-21 20:01:47 +0100

    feat: export mjs and cjs (#155)
    * feat: export mjs and cjs
    
    replaces interop cjs
    
    * build just like we would on release
    
    * fix jest tests
    
    * Add smoke test for kcd-scripts build
    
    * fix wpt tests

--- .babelrc.js
+++ .babelrc.js
@@ -1,6 +1,23 @@
 module.exports = {
 	presets: [
-		require.resolve("@babel/preset-env"),
+		[
+			require.resolve("@babel/preset-env"),
+			// for jest we need to transpile esmodules
+			// otherwise transform-commonjs takes care of the correct module syntax
+			{ modules: process.env.NODE_ENV === "test" ? "auto" : false }
+		],
 		require.resolve("@babel/preset-typescript")
-	]
+	],
+	env: {
+		cjs: {
+			plugins: [
+				[
+					require.resolve("@babel/plugin-transform-modules-commonjs"),
+					// helps rollup identify exports but also means __esmodule is enumerated
+					// not aware of a use case for enumerating imports though
+					{ loose: true }
+				]
+			]
+		}
+	}
 };
