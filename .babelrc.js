fe2017ff89df6e67e2ddef78ab969514c5cf71a8
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-03-21 19:01:53 +0100

    feat: export mjs and cjs
    replaces interop cjs

--- .babelrc.js
+++ .babelrc.js
@@ -1,6 +1,17 @@
 module.exports = {
 	presets: [
-		require.resolve("@babel/preset-env"),
+		[require.resolve("@babel/preset-env"), { modules: false }],
 		require.resolve("@babel/preset-typescript")
-	]
+	],
+	env: {
+		cjs: {
+			plugins: [
+				[
+					require.resolve("@babel/plugin-transform-modules-commonjs"),
+					// helps rollup identify exports
+					{ loose: true, strict: true }
+				]
+			]
+		}
+	}
 };
