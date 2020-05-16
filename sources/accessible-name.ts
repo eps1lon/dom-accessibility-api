ee9cc54b2812c81dc61e89684327e34c408aa5fc
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-05-16 13:04:44 +0200

    chore: document wpt vs accname conflict concerning child nodes (#236)

--- sources/accessible-name.ts
+++ sources/accessible-name.ts
@@ -133,15 +133,6 @@ function idRefs(node: Node, attributeName: string): Element[] {
 }
 
 /**
- * All defined children. This include childNodes as well as owned (portaled) trees
- * via aria-owns
- * @param node
- */
-function queryChildNodes(node: Node): Node[] {
-	return ArrayFrom(node.childNodes).concat(idRefs(node, "aria-owns"));
-}
-
-/**
  * @param {Node} node -
  * @returns {boolean} - As defined in step 2E of https://w3c.github.io/accname/#mapping_additional_nd_te
  */
@@ -315,7 +306,12 @@ export function computeAccessibleName(
 			accumulatedText = `${beforeContent} ${accumulatedText}`;
 		}
 
-		for (const child of queryChildNodes(node)) {
+		// FIXME: This is not defined in the spec
+		// But it is required in the web-platform-test
+		const childNodes = ArrayFrom(node.childNodes).concat(
+			idRefs(node, "aria-owns")
+		);
+		for (const child of childNodes) {
 			const result = computeTextAlternative(child, {
 				isEmbeddedInLabel: context.isEmbeddedInLabel,
 				isReferenced: false,
