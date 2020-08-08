c334a9085efd7f16f3c4cd645faeb7bd0d1200fd
Sebastian Silbermann <silbermann.sebastian@gmail.com>
2020-08-08 21:56:33 +0200

    fix ci coverage result

--- scripts/jest/jest.ci.config.js
+++ scripts/jest/jest.ci.config.js
@@ -3,6 +3,9 @@ const base = require("./jest.config");
 module.exports = {
 	...base,
 	collectCoverage: true,
+	// Needs to be defined or coverage from different projects won't be added
+	// See https://github.com/facebook/jest/issues/4255#issuecomment-321939025
+	collectCoverageFrom: ["**/*.ts"],
 	coverageReporters: ["cobertura"],
 	reporters: ["default", "jest-junit"],
 };
