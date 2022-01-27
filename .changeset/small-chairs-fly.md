cb3877882c35b474fe25f04365f4c0af11e10fa6
Caleb Eby <calebeby@users.noreply.github.com>
2022-01-27 11:33:16 -0800

    fix: `input[type="number"]` maps to `[role="spinbutton"]` (#796)
    Co-authored-by: Sebastian Silbermann <silbermann.sebastian@gmail.com>

---
+++ .changeset/small-chairs-fly.md
@@ -0,0 +1,5 @@
+---
+"dom-accessibility-api": patch
+---
+
+`<input type="number" />` now maps to `role` `spinbutton` (was `textbox` before).
--- sources/getRole.ts
+++ sources/getRole.ts
@@ -176,6 +176,8 @@ function getImplicitRole(element: Element): string | null {
 						return "combobox";
 					}
 					return "searchbox";
+				case "number":
+					return "spinbutton";
 				default:
 					return null;
 			}
