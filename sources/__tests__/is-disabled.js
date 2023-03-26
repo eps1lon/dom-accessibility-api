e051f9cdcd003528c442eb8dfe7c005c9c0fbcbd
Matan Borenkraout <matanbobi@gmail.com>
2023-03-26 10:51:57 +0300

    fix tests

--- sources/__tests__/is-disabled.js
+++ sources/__tests__/is-disabled.js
@@ -4,12 +4,12 @@ import { cleanup, renderIntoDocument } from "./helpers/test-utils";
 describe("isInaccessible", () => {
 	afterEach(cleanup);
 	test.each([
-		["<div data-test />", false],
-		['<div data-test aria-disabled="true" />', true],
-		['<div data-test disabled="true" />', true],
-		["<div data-test disabled />", true],
-		['<div data-test disabled="false" />', false],
-		['<div data-test aria-disabled="false" />', false],
+		["<button data-test />", false],
+		['<button data-test aria-disabled="true" />', true],
+		['<button data-test disabled="true" />', true],
+		["<button data-test disabled />", true],
+		['<button data-test aria-disabled="false" />', false],
+		["<div data-test disabled />", false],
 	])("markup #%#", (markup, expectedIsDisabled) => {
 		const container = renderIntoDocument(markup);
 		expect(container).not.toBe(null);
