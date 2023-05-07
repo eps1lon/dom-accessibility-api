215955ee5ea3582d49471f327291924deb44ff42
Matan Borenkraout <Matanbobi@gmail.com>
2023-05-07 23:02:16 +0300

    feat: add an isDisabled function to identify whether elements are disabled or not (#919)
    Co-authored-by: Sebastian Silbermann <silbermann.sebastian@gmail.com>

---
+++ .changeset/rare-cycles-nail.md
@@ -0,0 +1,5 @@
+---
+"dom-accessibility-api": minor
+---
+
+add an `isDisabled` function to check if elements are disabled or not
---
+++ sources/__tests__/is-disabled.js
@@ -0,0 +1,20 @@
+import { isDisabled } from "../is-disabled";
+import { cleanup, renderIntoDocument } from "./helpers/test-utils";
+
+describe("isDisabled", () => {
+	afterEach(cleanup);
+	test.each([
+		["<button data-test />", false],
+		['<button data-test aria-disabled="true" />', true],
+		['<button data-test disabled="true" />', true],
+		["<button data-test disabled />", true],
+		['<button data-test aria-disabled="false" />', false],
+		["<div data-test disabled />", false],
+	])("markup #%#", (markup, expectedIsDisabled) => {
+		const container = renderIntoDocument(markup);
+		expect(container).not.toBe(null);
+
+		const testNode = container.querySelector("[data-test]");
+		expect(isDisabled(testNode)).toBe(expectedIsDisabled);
+	});
+});
